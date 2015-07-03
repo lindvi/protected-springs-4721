f964e978502c3938274b86b1970d45c3ea134f79
lindvi <charlie.lindviken@valtech.se>
2015-07-03 10:41:55 +0200

    Removed sqlite3 for herokus sake

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -5,6 +5,40 @@
       $(this).toggleClass('is-active');
       $('.holder').toggleClass('stacked');
     });
+
+    $('.down').on('click', function(){
+      var four = $('.fourth');
+      var third = $('.third');
+      var second = $('.second');
+      var first = $('.first');
+      var base = $('.base');
+      var pre = $('.base ~ .pre').first();
+
+      four.addClass('bottom').removeClass('fourth');
+      third.addClass('fourth').removeClass('third');
+      second.addClass('third').removeClass('second');
+      first.addClass('second').removeClass('first');
+      base.addClass('first').removeClass('base');
+      pre.addClass('base').removeClass('pre');
+    });
+
+
+    $('.up').on('click', function(){
+      var bottom = $('.bottom').last();
+      var four = $('.fourth');
+      var third = $('.third');
+      var second = $('.second');
+      var first = $('.first');
+      var base = $('.base');
+      var pre = $('.base ~ .pre').first();
+
+      bottom.addClass('fourth').removeClass('bottom');
+      four.addClass('third').removeClass('fourth');
+      third.addClass('second').removeClass('third');
+      second.addClass('first').removeClass('second');
+      first.addClass('base').removeClass('first');
+      base.addClass('pre').removeClass('base');
+    });
   });
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Proto::Application.routes.draw do
   resources :users
 
-  get '/framed' => 'static_pages#framed'
+  get '/stacked' => 'static_pages#framed'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
